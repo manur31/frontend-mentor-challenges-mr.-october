28e9eee3e22cc331fc9437e51912066f2fe42266
Manuel Rosario <manurosario31@gmail.com>
2025-03-24 06:48:27 -0400

    Primer commit

---
+++ age-calculator-app-main/main.js
@@ -0,0 +1,7 @@
+const dayInput = document.querySelector('#day').value;
+const monthInput = document.querySelector('#month').value;
+const yearInput = document.querySelector('#year').value;
+
+console.log(dayInput);
+console.log(monthInput);
+console.log(yearInput);
